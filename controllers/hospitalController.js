405cfc7c2f594fd0bda95dbf145e020643f4e911
Paras248 <paraspatil248@gmail.com>
2022-11-21 12:21:38 +0530

    created add record route

--- controllers/hospitalController.js
+++ controllers/hospitalController.js
@@ -45,3 +45,48 @@ exports.hospitalSignIn = BigPromise(async (req, res, next) => {
 
     cookieToken(hospital, res, "hospital");
 });
+
+exports.patientAddRecord = BigPromise(async (req, res, next) => {
+    const { patientId, doctorId, medicines, disease, description } = req.body;
+    const hospitalId = req.hospital;
+
+    if (!patientId || !doctorId || !medicines || !disease || !description) {
+        return next(
+            res.status(400).json({
+                success: false,
+                message: "All fields are required",
+            })
+        );
+    }
+
+    try {
+        const record = await prisma.records.create({
+            data: {
+                medicines,
+                disease,
+                description,
+                patientId,
+                doctorId,
+                hospitalId,
+            },
+        });
+
+        const hospital = await prisma.hospital.findUnique({
+            where: {
+                id: hospitalId,
+            },
+        });
+
+        req.hospital = hospital;
+
+        res.status(200).json({
+            success: true,
+            record,
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            message: "hospital id or patient id or hospital id is incorrect",
+        });
+    }
+});
--- routes/hospital.js
+++ routes/hospital.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const Router = express.Router();
-const { hospitalSignIn } = require("../controllers/hospitalController");
+const { hospitalSignIn, patientAddRecord } = require("../controllers/hospitalController");
 
 Router.route("/hospital/signin").post(hospitalSignIn);
+Router.route("/hospital/patient/record/add").post(patientAddRecord);
 
 module.exports = Router;
